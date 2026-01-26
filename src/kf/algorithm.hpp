92b8b5595b27ada762d987e65fc042124d59b11c
JamahaW <153020210+JamahaW@users.noreply.github.com>
2026-01-26 20:36:02 +0300

    add std::abs alias

--- src/kf/algorithm.hpp
+++ src/kf/algorithm.hpp
@@ -8,10 +8,11 @@
 
 namespace kf {
 
-using std::find;    ///< Find element in range (std implementation)
-using std::for_each;///< Apply function to each element in range (std implementation)
-using std::max;     ///< Return larger of two values (std implementation)
-using std::min;     ///< Return smaller of two values (std implementation)
+using std::find;
+using std::for_each;
+using std::max;
+using std::min;
+using std::abs;
 
 /// Constrain value between lower and upper bounds
 template<typename T> constexpr T clamp(const T &value, const T &low, const T &high) noexcept {
