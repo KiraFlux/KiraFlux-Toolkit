2592b63f0bee8a46b356cf8146f8cba225b0cfab
JamahaW <153020210+JamahaW@users.noreply.github.com>
2026-02-16 21:18:41 +0300

    MizLangBridge.hpp enchantments

--- src/kf/network/MizLangBridge.hpp
+++ src/kf/network/MizLangBridge.hpp
@@ -15,25 +15,29 @@
 
 namespace kf {
 
-template<typename Tlc, typename Trc, usize N> struct MizLangBridge {
-    using LocalCodeType = Tlc;
-    using RemoteCodeType = Trc;
-
+/// @brief Structural Serial P2P byte-packet protocol
+/// @tparam N Exact count of local instructions
+/// @tparam Tlc Type of local instruction code
+/// @tparam Trc Type of remote instruction code
+template<usize N, typename Tlc, typename Trc = Tlc> struct MizLangBridge {
     enum class Error : u8 {
-        UnknownInstruction,
-        InstructionCodeReadFail,
-        InstructionCodeWriteFail,
-        InstructionSendHandlerNotReady,// добавлено
-        InstructionArgumentReadFail,
-        InstructionArgumentWriteFail
+        Receiver_CodeNotExists,
+        Receiver_CodeReadFail,
+        Receiver_ArgumentReadFail,
+
+        Sender_CodeWriteFail,
+        Sender_FunctionNotReady,
+        Sender_ArgumentWriteFail
     };
 
-    using SendHandler = Function<Result<void, Error>(io::OutputStream &, void *)>;
-    using ReceiveHandler = Function<Result<void, Error>(io::InputStream &)>;
-    using InstructionTable = Array<ReceiveHandler, N>;
+    using LocalCodeType = Tlc;
+    using RemoteCodeType = Trc;
+    using SendFunctionType = Function<Result<void, Error>(io::OutputStream &, void *)>;
+    using ReceiveFunctionType = Function<Result<void, Error>(io::InputStream &)>;
+    using InstructionTableType = Array<ReceiveFunctionType, N>;
 
 private:
-    InstructionTable instructions;
+    InstructionTableType instructions;
     io::InputStream in;
     io::OutputStream out;
     RemoteCodeType next_code{0};
@@ -42,39 +46,37 @@ public:
     explicit MizLangBridge(
         io::InputStream input_stream,
         io::OutputStream output_stream,
-        InstructionTable instructions) noexcept :
+        InstructionTableType instructions) noexcept :
         in{std::move(input_stream)},
         out{std::move(output_stream)},
         instructions{std::move(instructions)} {}
 
     struct Instruction {
     private:
+        const SendFunctionType sender;
         io::OutputStream &out;
-        const SendHandler handler;
         const RemoteCodeType code;
 
     public:
-        Instruction(io::OutputStream &output_stream, RemoteCodeType code, SendHandler call_handler) noexcept :
-            out{output_stream}, handler{std::move(call_handler)}, code{code} {}
+        explicit Instruction(io::OutputStream &output_stream, RemoteCodeType code, SendFunctionType sender) noexcept :
+            out{output_stream}, sender{std::move(sender)}, code{code} {}
 
         Instruction(Instruction &&other) noexcept :
-            out{other.out}, handler{std::move(other.handler)}, code{other.code} {}
+            out{other.out}, sender{std::move(other.sender)}, code{other.code} {}
 
         kf_nodiscard Result<void, Error> send(void *args) noexcept {
-            if (not handler) {
-                return {Error::InstructionSendHandlerNotReady};
+            if (not sender) {
+                return {Error::Sender_FunctionNotReady};
             }
             if (not out.write(code)) {
-                return {Error::InstructionCodeWriteFail};
+                return {Error::Sender_CodeWriteFail};
             }
-            return handler(out, args);
+            return sender(out, args);
         }
-
-        Instruction() = delete;
     };
 
-    kf_nodiscard Instruction createInstruction(SendHandler handler) noexcept {
-        return Instruction{out, next_code++, std::move(handler)};
+    kf_nodiscard Instruction createInstruction(SendFunctionType sender_function) noexcept {
+        return Instruction{out, next_code++, std::move(sender_function)};
     }
 
     kf_nodiscard Result<void, Error> poll() noexcept {
@@ -83,13 +85,13 @@ public:
         auto code_option = in.read<LocalCodeType>();
 
         if (not code_option.hasValue()) {
-            return {Error::InstructionCodeReadFail};
+            return {Error::Receiver_CodeReadFail};
         }
 
         const auto code = code_option.value();
         if (code >= instructions.size()) {
             in.clean();
-            return {Error::UnknownInstruction};
+            return {Error::Receiver_CodeNotExists};
         }
 
         return instructions[code](in);
