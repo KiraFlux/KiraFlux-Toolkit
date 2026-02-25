e1d82fde25fc42c078e8651632b6f42ffbbe2c74
KiraFlux <pret2titio1n@gmail.com>
2026-02-25 23:50:42 +0300

    remove most of kf_* attributes

--- src/kf/attributes.hpp
+++ src/kf/attributes.hpp
@@ -4,27 +4,7 @@
 #pragma once
 
 #if defined(__cplusplus) and __cplusplus >= 201703L
-#define kf_if_constexpr if constexpr
-#else
-#define kf_if_constexpr if
-#endif
-
-#if defined(__cplusplus) and __cplusplus >= 201703L
 #define kf_nodiscard [[nodiscard]]
 #else
 #define kf_nodiscard
-#endif
-
-#if defined(__cplusplus) and __cplusplus >= 201703L
-#define kf_maybe_unused [[maybe_unused]]
-#else
-#define kf_maybe_unused
-#endif
-
-#if defined(__cplusplus) and __cplusplus >= 201402L
-#define kf_deprecated(msg) [[deprecated(msg)]]
-#elif defined(__GNUC__) || defined(__clang__)
-#define kf_deprecated(msg) __attribute__((deprecated(msg)))
-#else
-#define kf_deprecated(msg)
 #endif
