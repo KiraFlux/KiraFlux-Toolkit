c2c347ddd61971936c3df65404ee5e0649f209bc
KiraFlux <pret2titio1n@gmail.com>
2026-03-07 00:00:27 +0300

    Implement SPI static interface and spi::Tag

---
+++ src/kf/drivers/bus/spi/SPI.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "kf/drivers/bus/Bus.hpp"
+
+#include "kf/drivers/bus/spi/Tag.hpp"
+
+namespace kf::drivers::bus::spi {
+
+/// @brief CRTP base class for SPI bus implementations.
+/// @tparam Impl Concrete bus implementation (must inherit from this class).
+/// @note Inherits from spi::Tag and bus::Bus<Impl>, providing type identification
+///       and the standard bus interface.
+///       Concrete implementations must define
+///       a nested Node type and implement all required bus methods.
+template<typename Impl> struct SPI : spi::Tag, Bus<Impl> {
+    using Node = typename Impl::Node;
+};
+
+}// namespace kf::drivers::bus::spi
---
+++ src/kf/drivers/bus/spi/Tag.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace kf::drivers::bus::spi {
+
+struct Tag {};
+
+}// namespace kf::drivers::bus::spi
