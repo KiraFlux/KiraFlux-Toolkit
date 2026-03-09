323d7ff68c216fb5c569842d09ffb6f78d97a067
KiraFlux <pret2titio1n@gmail.com>
2026-03-09 16:47:19 +0300

    Where did it go?

---
+++ src/kf/drivers/bus/spi/node/Tag.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace kf::drivers::bus::spi::node {
+
+struct Tag {};
+
+}// namespace kf::drivers::bus::spi::node
