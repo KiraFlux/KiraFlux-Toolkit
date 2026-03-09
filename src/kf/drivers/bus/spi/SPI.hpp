a11ed4bd91365781355a58ca30a4b42ac49403c3
KiraFlux <pret2titio1n@gmail.com>
2026-03-09 16:41:22 +0300

    SPI: NodeImpl must inherit spi-specific Node base

--- src/kf/drivers/bus/spi/SPI.hpp
+++ src/kf/drivers/bus/spi/SPI.hpp
@@ -3,13 +3,18 @@
 
 #pragma once
 
+#include "kf/meta/type_check.hpp"
+
 #include "kf/drivers/bus/Bus.hpp"
 #include "kf/drivers/bus/spi/Tag.hpp"
+#include "kf/drivers/bus/spi/node/Tag.hpp"
 
 namespace kf::drivers::bus::spi {
 
 /// @brief CRTP base class for SPI bus implementations.
 /// @tparam Impl Concrete bus implementation (must inherit from this class).
-template<typename Impl, typename NodeImpl, typename ErrorImpl> struct SPI : spi::Tag, Bus<Impl, NodeImpl, ErrorImpl> {};
+template<typename Impl, typename NodeImpl, typename ErrorImpl> struct SPI : spi::Tag, Bus<Impl, NodeImpl, ErrorImpl> {
+    kf_crtp_check(NodeImpl, kf::drivers::bus::spi::node::Tag);
+};
 
 }// namespace kf::drivers::bus::spi
