d1493973e2cf09f895b4f665a6ee8e02b2f70ff4
KiraFlux <pret2titio1n@gmail.com>
2026-03-12 11:32:27 +0300

    sharp now uses Initable mixin

--- src/kf/drivers/sensors/Sharp.hpp
+++ src/kf/drivers/sensors/Sharp.hpp
@@ -6,21 +6,17 @@
 #include "kf/gpio/GPIO.hpp"
 #include "kf/math/units.hpp"
 #include "kf/meta/type_check.hpp"
+#include "kf/mixin/Initable.hpp"
 
 namespace kf::drivers::sensors {
 
 /// @brief Sharp infrared distance sensor driver
 /// @note Provides distance measurements in millimeters using analog voltage output
-template<typename I> struct Sharp {
+template<typename I> struct Sharp : kf::mixin::Initable<Sharp<I>, void> {
     kf_crtp_check(I, kf::gpio::AdcInputTag);
     using PinImpl = I;
 
     explicit Sharp(PinImpl &&pin) noexcept : _pin{pin} {}
-
-    /// @brief Initialize sensor hardware
-    void init() noexcept {
-        _pin.init();
-    }
 
     /// @brief Read distance in millimeters
     /// @return Distance in millimeters (inverse of voltage)
@@ -33,6 +29,11 @@ template<typename I> struct Sharp {
 
 private:
     PinImpl _pin;
+
+    // initable impl
+
+    friend struct kf::mixin::Initable<Sharp<I>, void>;
+    void initImpl() noexcept { _pin.init(); }
 };
 
 }// namespace kf::drivers::sensors
