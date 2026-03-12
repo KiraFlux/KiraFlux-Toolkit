cb3e2605de94ff9766f428378b2c7740350c2d74
KiraFlux <pret2titio1n@gmail.com>
2026-03-13 00:56:07 +0300

    Update shapshot.py

--- shapshot.py
+++ shapshot.py
@@ -3,24 +3,35 @@ from pathlib import Path
 
 sys.stdout = open("shapshot.txt", "wt", encoding='utf-8')
 
-repo_dir = Path(".")
+REPO_DIR = Path(".")
 
 target_ext = (
-    "h", "hpp", "cpp", "md", "ino", "yml", "sh"
+    "h", "hpp", "cpp", "md", "ino", "yml", "sh", "py"
 )
 
 target_dirs = (
     "examples", "src", "test", ".github"
 )
 
+def _write(f: Path):
+    try:
+        print(f"{file.relative_to(REPO_DIR)}")
+        print(f"```{f.suffix}")
+        print(file.read_text(encoding='utf-8', errors='ignore').rstrip())
+        print("```\n")
+    except:
+        pass
+
+_write(REPO_DIR / "makefile")
+
+for ext in target_ext:
+    for file in REPO_DIR.glob(f"*.{ext}"):
+        _write(file)
+
 for d in target_dirs:
-    _dir = repo_dir / d
+    _dir = REPO_DIR / d
     for ext in target_ext:
         for file in _dir.rglob(f"*.{ext}"):
-            try:
-                print(f"{file.relative_to(repo_dir)}")
-                print(f"```{ext}")
-                print(file.read_text(encoding='utf-8', errors='ignore').rstrip())
-                print("```\n")
-            except:
-                pass
+            _write(file)
+
+
