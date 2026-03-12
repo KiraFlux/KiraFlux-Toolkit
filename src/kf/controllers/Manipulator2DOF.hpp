851a8df1e3f37b73f3a434fd51349f5f1469a40f
KiraFlux <pret2titio1n@gmail.com>
2026-03-12 13:55:28 +0300

    Generalize Manipulator 2 dof

--- src/kf/controllers/Manipulator2DOF.hpp
+++ src/kf/controllers/Manipulator2DOF.hpp
@@ -3,74 +3,43 @@
 
 #pragma once
 
-#include "kf/drivers/zms/PwmPositionServo.hpp"
 #include "kf/math/units.hpp"
-#include "kf/validation.hpp"
 
 namespace kf::controllers {
 
 /// @brief Two-degree-of-freedom robotic manipulator with servo control
 /// @note Controls arm and claw axes using PWM-position servo drivers
-struct Manipulator2DOF {
+template<typename I> struct Manipulator2DOF {
+    using ServoImpl = I;
 
-    static constexpr auto logger{Logger::create("Encoder")};
-
-    /// @brief Configuration for 2DOF manipulator
-    struct Config : Validatable<Config> {
-        drivers::zms::PwmPositionServo::PwmConfig servo_pwm;                     ///< PWM signal
-        drivers::zms::PwmPositionServo::PulseConfig servo_generic_pulse_settings;///< Pulse timing
-        drivers::zms::PwmPositionServo::DriverConfig claw_axis;                  ///< Claw axis servo
-        drivers::zms::PwmPositionServo::DriverConfig arm_axis;                   ///< Arm axis servo
-
-        /// @brief Validate all configuration parameters
-        void checkImpl(Validator &validator) const noexcept {
-            kf_Validator_check(validator, logger, arm_axis.check());
-            kf_Validator_check(validator, logger, claw_axis.check());
-            kf_Validator_check(validator, logger, servo_pwm.check());
-            kf_Validator_check(validator, logger, servo_generic_pulse_settings.check());
-        }
-    };
-
-private:
-    const Config &_config;               ///< Reference to configuration
-    drivers::zms::PwmPositionServo _arm; ///< Arm axis servo driver
-    drivers::zms::PwmPositionServo _claw;///< Claw axis servo driver
-
-public:
     /// @brief Construct manipulator instance
-    explicit Manipulator2DOF(const Config &config) noexcept :
-        _config{config},
-        _arm{config.servo_pwm, config.arm_axis, config.servo_generic_pulse_settings},
-        _claw{config.servo_pwm, config.claw_axis, config.servo_generic_pulse_settings} {}
+    explicit Manipulator2DOF(ServoImpl &&arm, ServoImpl &&claw) noexcept : _arm{arm}, _claw{claw} {}
 
     /// @brief Initialize both servo axes
     /// @return true if both servos initialized successfully
-    /// @note Logs error message if initialization fails
     [[nodiscard]] bool init() noexcept {
-        if (not _arm.init()) {
-            logger.error("arm axis fail");
-            return false;
-        }
+        bool ok{false};
 
-        if (not _claw.init()) {
-            logger.error("claw axis fail");
-            return false;
-        }
+        if (not(ok = _arm.init())) { goto ret; }
+        if (not(ok = _claw.init())) { goto ret; }
 
-        return true;
+        return ret;
     }
 
     /// @brief Set arm axis angle
-    void arm(math::Degrees angle) noexcept { _arm.set(angle); }
+    void arm(math::Degrees angle) noexcept { _arm.write(angle); }
 
     /// @brief Set claw axis angle
-    void claw(math::Degrees angle) noexcept { _claw.set(angle); }
+    void claw(math::Degrees angle) noexcept { _claw.write(angle); }
 
     /// @brief Disable arm axis servo (stop PWM)
     void disableArm() noexcept { _arm.disable(); }
 
     /// @brief Disable claw axis servo (stop PWM)
     void disableClaw() noexcept { _claw.disable(); }
+
+private:
+    ServoImpl _arm, _claw;
 };
 
 }// namespace kf::controllers
