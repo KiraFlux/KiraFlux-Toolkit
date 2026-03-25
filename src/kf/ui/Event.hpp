0f9edbe7bd5a740e1653075895c3ad9c686d2efd
KiraFlux <pret2titio1n@gmail.com>
2026-03-25 16:30:06 +0300

    upd ui/Event.hpp

--- src/kf/ui/Event.hpp
+++ src/kf/ui/Event.hpp
@@ -9,11 +9,13 @@
 
 namespace kf::ui {
 
+struct EventTag {};
+
 /// @brief Incoming UI event with kind and value packed into single byte
-/// @tparam V Number of bits for event value
-template<usize V> struct Event {
+/// @tparam N Number of bits for event value
+template<usize N> struct Event : EventTag {
     static constexpr auto type_bits{2u};
-    static constexpr auto value_bits{V};
+    static constexpr auto value_bits{N};
     static constexpr auto total_bits{static_cast<usize>(type_bits + value_bits)};
 
     using Value = typename meta::BitTraits<value_bits>::min_signed;
@@ -21,19 +23,13 @@ template<usize V> struct Event {
     using UnsignedValue = typename meta::BitTraits<value_bits>::min_unsigned;
 
     static constexpr auto storage_bits{static_cast<usize>(sizeof(Storage) * 8)};
-
     static constexpr auto value_max{static_cast<Value>((UnsignedValue{1u} << (value_bits - 1)) - 1)};
     static constexpr auto value_min{static_cast<Value>(-value_max - 1)};
-
-private:
     static constexpr auto event_value_full{static_cast<Storage>(~Storage{0})};
     static constexpr auto value_mask{static_cast<Storage>((Storage{1} << value_bits) - 1)};
-    static constexpr auto type_mask{static_cast<Storage>(event_value_full & ~value_mask)};
+    static constexpr auto type_mask{static_cast<Storage>(event_value_full & (~value_mask))};
     static constexpr auto sign_bit_mask{static_cast<Storage>(Storage{1} << (value_bits - 1))};
 
-    Storage _storage;
-
-public:
     enum class Kind : Storage {
         Update = static_cast<Storage>(Storage{0} << value_bits),
         PageCursorMove = static_cast<Storage>(Storage{1} << value_bits),
@@ -41,8 +37,8 @@ public:
         WidgetValueChange = static_cast<Storage>(Storage{3} << value_bits),
     };
 
-    constexpr explicit Event(Kind kind, Value value = 0) noexcept
-        : _storage{static_cast<Storage>((static_cast<Storage>(kind) & type_mask) | static_cast<Storage>((clamp(value, value_min, value_max)) & value_mask))} {}
+    constexpr explicit Event(Kind kind, Value value = 0) noexcept :
+        _storage{static_cast<Storage>((static_cast<Storage>(kind) & type_mask) | static_cast<Storage>((clamp(value, value_min, value_max)) & value_mask))} {}
 
     [[nodiscard]] constexpr Kind kind() const noexcept {
         return static_cast<Kind>(_storage & type_mask);
@@ -61,6 +57,9 @@ public:
     [[nodiscard]] static constexpr Event widgetValue(Value value) noexcept {
         return Event{Kind::WidgetValueChange, value};
     }
+
+private:
+    Storage _storage;
 };
 
 }// namespace kf::ui
