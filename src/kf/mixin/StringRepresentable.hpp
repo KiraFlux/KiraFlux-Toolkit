c3aa09aebbc840d19fe232e2cb2955301ce12c41
KiraFlux <pret2titio1n@gmail.com>
2026-05-18 21:46:18 +0300

    Feature: StringRepresentable mixin Fixes #153

---
+++ src/kf/mixin/StringRepresentable.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "kf/memory/ArrayString.hpp"
+#include "kf/primitives.hpp"
+
+namespace kf::mixin {
+
+struct StringRepresentableTag {};
+
+template<typename Impl, kf::usize> struct StringRepresentable : StringRepresentableTag {
+    static constexpr auto repr_string_capacity{N};
+
+    [[nodiscard]] memory::ArrayString<N> toString() const noexcept {
+        return static_cast<const Impl *>(this)->toStringImpl();
+    }
+};
+
+}// namespace kf::mixin
+
+#define KF_IMPL_STRING_REPRESENTABLE(__impl__, n) friend struct ::kf::mixin::StringRepresentable<__impl__, n>
