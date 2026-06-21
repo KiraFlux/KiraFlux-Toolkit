090304aec5499239022194ed4a769bc900946126
KiraFlux <pret2titio1n@gmail.com>
2026-06-21 17:15:02 +0300

    feat(ui/decoration): add spacer

--- src/kf/ui/Decoration.hpp
+++ src/kf/ui/Decoration.hpp
@@ -11,8 +11,9 @@ namespace kf::ui {
 ///       The renderer maps each decoration to an appropriate symbol or glyph.
 ///       For monochrome or limited displays, the renderer may substitute with a simple character.
 enum class Decoration : char {
-    Arrow,///< Indicates navigation, expansion, or "go to" action.
-    Colon,///< Separator between a label and its value (e.g., "Name: value").
+    Space = 0x00,///< Horizontal space between primitives
+    Arrow = 0x01,///< Indicates navigation, expansion, or "go to" action.
+    Colon = 0x02,///< Separator between a label and its value (e.g., "Name: value").
 };
 
 }// namespace kf::ui
