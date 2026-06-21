3b15ce03e8a6d89b4347b28eff4911f4ebeb3e33
KiraFlux <pret2titio1n@gmail.com>
2026-06-21 16:02:22 +0300

    refactor(ui/widgets/combo-box): use combo item as callback argument

--- src/kf/ui/widgets/ComboBox.hpp
+++ src/kf/ui/widgets/ComboBox.hpp
@@ -66,7 +66,7 @@ template<typename U, typename T> struct ComboBox :
 
     ComboBoxTag,
     U::Widget,
-    mixin::Callbacked<T>,
+    mixin::Callbacked<internal::ComboBoxItem<T>>,
     mixin::Configurable<internal::ComboBoxConfig<T>>
 
 {
@@ -100,7 +100,7 @@ template<typename U, typename T> struct ComboBox :
     /// @param value Navigation direction (positive/negative)
     [[nodiscard]] bool onEventValue(typename U::EventImpl::Value event_value) noexcept override {
         moveCursor(event_value);
-        this->invoke(this->config().items[_cursor].value());
+        this->invoke(this->config().items[_cursor]);
         return true;// redraw required after selection change
     }
 
