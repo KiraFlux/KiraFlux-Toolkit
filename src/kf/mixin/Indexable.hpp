b84a544b37c6b307a793bf143273546e64877a3f
KiraFlux <pret2titio1n@gmail.com>
2026-06-28 16:31:53 +0300

    feat(mixin): add Indexable mixin

---
+++ src/kf/mixin/Indexable.hpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace kf::mixin {
+
+struct IndexableTag {};
+
+/// @brief Adds Index operator
+/// @tparam Impl Indexable implementation class with `constexpr T getItemImpl(usize index) noexcept` method
+/// @tparam T item type
+template<typename Impl, typename T> struct Indexable : IndexableTag {
+
+    /// @brief Get mutable access to item by index
+    [[nodiscard]] constexpr T &operator[](usize index) noexcept {
+        return static_cast<Impl *>(this)->getItemImpl(index);
+    }
+
+    /// @brief Get readonly access to item by index
+    [[nodiscard]] constexpr const T &operator[](usize index) const noexcept {
+        return const_cast<Impl *>(this)->getItemImpl(index);
+    }
+};
+
+}// namespace kf::mixin
+
+#define KF_IMPL_INDEXABLE(__impl__, __type__) friend struct ::kf::mixin::Indexable<__impl__, __type__>
