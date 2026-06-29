9e8cf5e59fa25c5a93fe99b20324fec0350a11d8
KiraFlux <pret2titio1n@gmail.com>
2026-06-29 14:41:19 +0300

    wip(test): add array init test

---
+++ test/unit/common/test_array/array.cpp
@@ -0,0 +1,16 @@
+#include <kf/Array.hpp>
+#include <runner.hpp>
+
+void test() {
+    kf::Array<int, 4> array{{1, 2, 3, 4}};
+
+    TEST_ASSERT_TRUE(array[0] == 1);
+    TEST_ASSERT_TRUE(array[1] == 2);
+    TEST_ASSERT_TRUE(array[2] == 3);
+    TEST_ASSERT_TRUE(array[3] == 4);
+}
+
+void run_tests() {
+    RUN_TEST(test);
+}
+
