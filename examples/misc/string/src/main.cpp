2488904ab35d0dfc5c2762a90c50ca3a03d82ee1
KiraFlux <pret2titio1n@gmail.com>
2026-07-01 17:25:49 +0300

    feat: update misc/string example

--- examples/misc/string/src/main.cpp
+++ examples/misc/string/src/main.cpp
@@ -7,15 +7,62 @@
 void setup() {
     Serial.begin(115200);
 
-    char buffer[100];
+    char buffer[256];
+    kf::String my_string{{buffer}}; // construct from slice from array
 
-    kf::Slice<char> my_buffer_view{buffer};
+    // formatting: use {} as anchor, {{ or }} to display '{' or '}'
+    my_string.format(
 
-    kf::String my_string{my_buffer_view};
+        "{{ hello }} {} {} {} {} {} {} {}\n",
+        kf::StringView{"string-view"},
+        "c-string",
+        123,  // integer
+        6.7,  // float-point
+        true, // boolean: true
+        false,// boolean: false
+        'H'   // char
 
-    my_string.format("{{ hello }} {}", 123);
-    
+    );
+
     Serial.write(my_string.data(), my_string.length());
+
+    Serial.printf(
+
+        "Length/Capacity: %d/%d\n",
+        my_string.length(), // current length
+        my_string.capacity()// maximal length
+
+    );
+
+    // clear (just set head to zero)
+    my_string.reset();
+
+    // append
+
+    // any Sequence<char>
+    my_string.append(kf::StringView{"sv"});
+    my_string.append("c-str");
+    // my_string.append();
+
+    // booleans
+    my_string.append(true);
+    my_string.append(false);
+
+    // numbers
+    my_string.append(kf::i64{13456789});
+    my_string.append(0.4 - 0.3, 50); // precision
+
+    // char
+    my_string.append('c');
+
+    // from Stack<char>
+    const bool ok = my_string.push('c');
+    const kf::Option<char> popped = my_string.pop();
+
+    // convert to c-style string - add terminating null
+    Serial.print(my_string.cString());
+
+    delay(5000);
 }
 
 void loop() {}
