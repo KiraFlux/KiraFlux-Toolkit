3e79add8820ee5d5c5eab08be85347774ef007c3
KiraFlux <pret2titio1n@gmail.com>
2026-07-01 23:36:52 +0300

    fix(test/array): use aggregate init

--- test/unit/common/test_array/array.cpp
+++ test/unit/common/test_array/array.cpp
@@ -2,7 +2,7 @@
 #include <runner.hpp>
 
 void test() {
-    kf::Array<int, 4> array{{1, 2, 3, 4}};
+    kf::Array<int, 4> array{.items = {1, 2, 3, 4}};
 
     TEST_ASSERT_TRUE(array[0] == 1);
     TEST_ASSERT_TRUE(array[1] == 2);
@@ -13,4 +13,3 @@ void test() {
 void run_tests() {
     RUN_TEST(test);
 }
-
