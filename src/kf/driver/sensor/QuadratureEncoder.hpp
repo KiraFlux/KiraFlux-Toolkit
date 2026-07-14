1fce38508502b4767ccfc3c6768cca4bc7f2dac9
KiraFlux <pret2titio1n@gmail.com>
2026-07-14 23:28:49 +0300

    refactor(driver/sensor/quadrature-encoder): remove Arduino dependency. replace static table with switch

--- src/kf/driver/sensor/QuadratureEncoder.hpp
+++ src/kf/driver/sensor/QuadratureEncoder.hpp
@@ -3,12 +3,13 @@
 
 #pragma once
 
-#include <Arduino.h>
-
-#include "kf/driver/sensor/SensorDriver.hpp"
+#include "kf/GPIO.hpp"
+#include "kf/concepts.hpp"
 #include "kf/mixin/Configured.hpp"
 #include "kf/mixin/Resettable.hpp"
 #include "kf/primitives.hpp"
+
+#include "kf/driver/sensor/SensorDriver.hpp"
 
 #ifndef IRAM_ATTR
 #define IRAM_ATTR
@@ -20,11 +21,10 @@ namespace kf::internal {
 /// @tparam T Physical unit type representing linear distance per tick
 template<typename T> struct QuadratureEncoderConfig final {
 
-    using TickType = kf::i32;      ///< Integral tick counter type
+    using TickType = kf::isize;    ///< Integral tick counter type
     using UnitType = T;            ///< Physical unit type
     using StepType = kf::i8;       ///< Direction step type
     using PhaseStateType = kf::u32;///< Packed two-bit phase state (AB)
-    using GpioNumType = kf::u8;    ///< GPIO number type
 
     /// @brief Rotation direction that increments the tick counter
     /// @note Follows the right-hand grip rule
@@ -33,9 +33,8 @@ template<typename T> struct QuadratureEncoderConfig final {
         CCW = +1///< Counter-clockwise increments
     };
 
-    UnitType units_per_tick;                       ///< Conversion factor: physical units per encoder tick
-    Direction positive_direction;                  ///< Desired positive rotation direction
-    GpioNumType gpio_num_phase_a, gpio_num_phase_b;///< GPIO pins for phase A and phase B
+    UnitType units_per_tick;     ///< Conversion factor: physical units per encoder tick
+    Direction positive_direction;///< Desired positive rotation direction
 
     /// @brief Converts ticks to physical units
     [[nodiscard]] constexpr UnitType unitsFromTicks(TickType value) const noexcept {
@@ -56,16 +55,19 @@ namespace kf::driver::sensor {
 /// @brief Quadrature encoder sensor with 4X decoding
 /// @note Arduino-Only
 /// @tparam T Physical linear unit
-template<typename T> struct QuadratureEncoder final :
+template<implements<GPIO::DigitalInputTag> G, typename T> struct QuadratureEncoder final :
 
-    driver::sensor::SensorDriver<QuadratureEncoder<T>, typename internal::QuadratureEncoderConfig<T>::PhaseStateType, void()>,
-    mixin::Resettable<QuadratureEncoder<T>>,
+    driver::sensor::SensorDriver<QuadratureEncoder<G, T>, typename internal::QuadratureEncoderConfig<T>::PhaseStateType, void()>,
+    mixin::Resettable<QuadratureEncoder<G, T>>,
     mixin::Configured<internal::QuadratureEncoderConfig<T>>
 
 {
+    using DigitalInputImpl = G;
+
     using Config = internal::QuadratureEncoderConfig<T>;
 
-    using mixin::Configured<Config>::Configured;
+    explicit constexpr QuadratureEncoder(const Config &config, DigitalInputImpl &&gpio_phase_a, DigitalInputImpl &&gpio_phase_b) noexcept :
+        mixin::Configured<Config>::Configured{config}, _gpio_phase_a{std::move(gpio_phase_a)}, _gpio_phase_b{std::move(gpio_phase_b)} {}
 
     /// @brief Current accumulated position in ticks
     [[nodiscard]] auto positionTicks() const noexcept -> typename Config::TickType {
@@ -88,55 +90,55 @@ template<typename T> struct QuadratureEncoder final :
     }
 
 private:
+    DigitalInputImpl _gpio_phase_a, _gpio_phase_b;
     volatile typename Config::TickType _position_ticks{0};  ///< Accumulated step count
     volatile typename Config::PhaseStateType _last_state{0};///< Previous AB phase state
 
-    /// @brief ISR triggered on any edge of either phase
-    static void IRAM_ATTR onAnyPhaseChange(void *arg) {
-        // 4X decoding lookup table (index = (prev_A << 3 | prev_B << 2 | cur_A << 1 | cur_B))
-        static const typename Config::StepType increment_table[16] = {
-            0, // 00 -> 00 : invalid
-            +1,// 00 -> 01 : CW step
-            -1,// 00 -> 10 : CCW step
-            0, // 00 -> 11 : invalid
-            -1,// 01 -> 00 : CCW step
-            0, // 01 -> 01 : invalid
-            0, // 01 -> 10 : invalid
-            +1,// 01 -> 11 : CW step
-            +1,// 10 -> 00 : CW step
-            0, // 10 -> 01 : invalid
-            0, // 10 -> 10 : invalid
-            -1,// 10 -> 11 : CCW step
-            0, // 11 -> 00 : invalid
-            -1,// 11 -> 01 : CCW step
-            +1,// 11 -> 10 : CW step
-            0  // 11 -> 11 : invalid
-        };
+    using This = QuadratureEncoder<G, T>;
 
-        auto &self = *static_cast<QuadratureEncoder *>(arg);
+    /// @brief ISR triggered on any edge of either phase
+    static void IRAM_ATTR onAnyPhaseChange(void *arg) noexcept {
+        auto &self = *static_cast<This *>(arg);
+        const auto positive_step = static_cast<typename Config::StepType>(self.config().positive_direction);
+
         const auto current_state = self.read();
 
-        // Index formed by concatenating previous and current states (4 bits)
-        self._position_ticks += increment_table[(current_state | (self._last_state << 2))] * static_cast<typename Config::StepType>(self.config().positive_direction);
+        // Index formed by concatenating last and current states (4 bits)
+        // 4X decoding lookup table (index = (last_A << 3 | last_B << 2 | cur_A << 1 | cur_B))
+        switch ((self._last_state << 2) | current_state) {
+            case 0b00'01:
+            case 0b01'11:
+            case 0b10'00:
+            case 0b11'10:
+                self._position_ticks += positive_step;
+                break;
+
+            case 0b00'10:
+            case 0b01'00:
+            case 0b10'11:
+            case 0b11'01:
+                self._position_ticks -= positive_step;
+                break;
+        }
+
         self._last_state = current_state;
     }
 
-    // Implementation details
-    KF_IMPL_SENSOR_DRIVER(QuadratureEncoder<T>, typename Config::PhaseStateType, void());
+    KF_IMPL_SENSOR_DRIVER(This, typename Config::PhaseStateType, void());
 
     void initImpl() noexcept {
-        pinMode(this->config().gpio_num_phase_a, INPUT);
-        attachInterruptArg(digitalPinToInterrupt(this->config().gpio_num_phase_a), onAnyPhaseChange, static_cast<void *>(this), CHANGE);
+        _gpio_phase_a.init();
+        _gpio_phase_a.attachInterrupt(onAnyPhaseChange, static_cast<void *>(this), DigitalInputImpl::Interrupt::OnChange);
 
-        pinMode(this->config().gpio_num_phase_b, INPUT);
-        attachInterruptArg(digitalPinToInterrupt(this->config().gpio_num_phase_b), onAnyPhaseChange, static_cast<void *>(this), CHANGE);
+        _gpio_phase_b.init();
+        _gpio_phase_b.attachInterrupt(onAnyPhaseChange, static_cast<void *>(this), DigitalInputImpl::Interrupt::OnChange);
 
         this->reset();
     }
 
     typename Config::PhaseStateType readImpl() const noexcept {
-        const auto state_a = static_cast<typename Config::PhaseStateType>(digitalRead(this->config().gpio_num_phase_a));
-        const auto state_b = static_cast<typename Config::PhaseStateType>(digitalRead(this->config().gpio_num_phase_b));
+        const auto state_a = static_cast<typename Config::PhaseStateType>(_gpio_phase_a.read());
+        const auto state_b = static_cast<typename Config::PhaseStateType>(_gpio_phase_b.read());
         return (state_a << 1) | state_b;// pack as AB
     }
 
