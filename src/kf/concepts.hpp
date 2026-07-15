23130da52747d7b17778c8c098da6adc43a06614
KiraFlux <pret2titio1n@gmail.com>
2026-07-15 03:05:32 +0300

    feat: add arithmetic concept

--- src/kf/concepts.hpp
+++ src/kf/concepts.hpp
@@ -4,10 +4,14 @@
 #pragma once
 
 #include <concepts>
+#include <type_traits>
 
 namespace kf {
 
 template<typename Impl, typename Tag>
 concept implements = std::derived_from<Impl, Tag>;
+
+template<typename T>
+concept arithmetic = std::is_arithmetic_v<T>;
 
 }// namespace kf
