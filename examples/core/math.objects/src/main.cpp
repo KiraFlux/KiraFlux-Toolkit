b878353119b9ced4d980ace348ed84bf605a146d
KiraFlux <pret2titio1n@gmail.com>
2026-07-17 01:28:21 +0300

    refactor(examples/math.objects): use kf::main, add quaternion demo

--- examples/core/math.objects/src/main.cpp
+++ examples/core/math.objects/src/main.cpp
@@ -1,18 +1,20 @@
 // KiraFlux-Toolkit Demo 'vectors'
-#include <Arduino.h>
 
+#include <kf/main.hpp>
 #include <kf/math.hpp>
 
 using namespace kf::math;
 
-void setup() {
-    Serial.begin(115200);
+void kf::main(kf::Init &) {
+    
+    // vector 2d
     {
         Vector2<float> a1{.x = 3.0f, .y = 4.0f};// aggregate initialization
-        Serial.println(a1.length());            // len: 5.0
+        a1.length();                            // len: 5.0
+        auto zero = Vector2f::zero();
 
         const auto a2 = Vector2<float>::create(12, 5.0);// create with shortcut for static casts
-        Serial.println(a2.length());                    // len: 13.0
+        a2.length();                                    // len: 13.0
 
         auto sum = a1 + a2;
         auto sub = a1 - a2;
@@ -22,8 +24,15 @@ void setup() {
         auto div_by_scalar_checked_none = a1.divChecked(0);// Option(None)
         auto scalar_mul = a1.dot(a2);
         auto normalized = a1.normalized();// None if a1.length() is 0
+
+        // Vector2i = Vector2<i32>
+        // Vector2f = Vector2<f32>
     }
+
+    // vector 3d
     {
+
+        // construct
         Vector3<kf::u8> b1{.x = 1, .y = 2, .z = 3};
         const auto b2 = Vector3<kf::u8>::create(4, 5.0f, 6.0);
         auto zero{Vector3<double>::zero()};
@@ -39,8 +48,33 @@ void setup() {
         auto normalized = b1.normalized();// None if b1.length() is 0
     }
 
-    // Vector2i = Vector2<i32>
-    // Vector2f = Vector2<f32>
+    // quaternion
+    {
+
+        // construct
+
+        Quaternion<float> q1{
+            .x = 0,
+            .y = 0,
+            .z = 0,
+            .w = 1,
+        };
+
+        const auto q2 = Quaternion<double>::create(1, 2.0f, 3.0, 0);
+
+        const auto i = Quaternion<float>::identity();
+        
+        const auto c = q1.conjugate();
+        const auto v = q1.inverse();
+        const auto n = q1.normalized();
+
+        q1.normalize(); // applied to self
+        
+        const auto l = q1.length();
+        const auto l2 = q1.lengthSquared();
+        const auto rotated = q2.rotate(Vector3f::create(1, 0, 0));
+        const auto e = q2.toEulers();
+    }
 }
 
 void loop() {}
