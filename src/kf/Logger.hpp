1a87510e674667db6669f12a7f669e432613c219
KiraFlux <pret2titio1n@gmail.com>
2026-07-19 19:18:28 +0300

    feat(logger): add optional buffering and compile-time formatting

--- src/kf/Logger.hpp
+++ src/kf/Logger.hpp
@@ -3,48 +3,80 @@
 
 #pragma once
 
+#include "kf/Slice.hpp"
+#include "kf/Stack.hpp"
 #include "kf/StringView.hpp"
 #include "kf/mixin/NonCopyable.hpp"
+#include "kf/mixin/Writable.hpp"
 
 namespace kf {
 
 /// @brief Logging system for embedded applications
-struct Logger : mixin::NonCopyable {
+struct Logger :
+
+    mixin::NonCopyable,
+    private mixin::Writable<Logger, char>
+
+{
 
     using WriteHandler = void (*)(StringView);
 
     /// @brief Current output handler (nullptr disables logging)
     inline static WriteHandler writer{nullptr};
 
-    explicit constexpr Logger(StringView key) noexcept :
-        _key{key} {}
+    explicit constexpr Logger(StringView key, Slice<char> buffer = {}) noexcept :
+        _key{key}, _stack{buffer} {}
 
-#define MAKE(__level__)                                  \
-    void __level__(StringView message) const noexcept {  \
-        write(StringView{":" #__level__ "] "}, message); \
+    /// @brief Get Logger key
+    [[nodiscard]] constexpr StringView key() const noexcept {
+        return _key;
     }
 
-    MAKE(info)
+#define MAKE(__kf_level__, __kf_level_name__)                                                                         \
+    template<typename... Args> void __kf_level__(internal::FormatString<Args...> fmt, const Args &...args) noexcept { \
+        this->append('[');                                                                                            \
+        this->append(_key);                                                                                           \
+        this->append(":" __kf_level_name__ "] ");                                                                     \
+        this->format(fmt, args...);                                                                                   \
+        this->append('\n');                                                                                           \
+        flush();                                                                                                      \
+    }
 
-    MAKE(warn)
-
-    MAKE(error)
-
-    MAKE(debug)
+    MAKE(debug, "D")
+    MAKE(info, "I")
+    MAKE(warn, "W")
+    MAKE(error, "E")
 
 #undef MAKE
 
+    /// @brief Write all buffered chars
+    void flush() noexcept {
+        writer({_stack.slice()});
+        _stack.reset();
+    }
+
 private:
+    Stack<char> _stack;
     StringView _key;
 
-    void write(StringView level, StringView message) const noexcept {
-        if (nullptr != writer) {
-            writer("[");
-            writer(_key);
-            writer(level);
-            writer(message);
-            writer("\n");
+    KF_IMPL_WRITABLE(Logger, char);
+    bool writeImpl(char c) noexcept {
+        if (nullptr == writer) {
+            return false;
         }
+
+        // bufferless mode -> direct write
+        if (_stack.capacity() == 0) {
+            writer({&c, 1});
+        } else {
+            if (_stack.full()) {
+                flush();
+            }
+
+            (void) _stack.write(c);
+        }
+
+        return true;
     }
 };
 
