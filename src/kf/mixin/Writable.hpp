1e902fe29cce25dd6d71043513ffd91db5374451
KiraFlux <pret2titio1n@gmail.com>
2026-07-19 16:26:00 +0300

    feat(mixin): basic Writable<Impl, T> implementation

---
+++ src/kf/mixin/Writable.hpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <utility>
+
+namespace kf::mixin {
+
+struct WritableTag {};
+
+}// namespace kf::mixin
+
+namespace kf::internal {
+
+template<typename Impl, typename T> struct WritableBase : mixin::WritableTag {
+
+    /// @brief Write Value
+    /// @param value Value to write (perfect-forwarded)
+    /// @return `true` if write successful, `false` otherwise
+    [[noexcept]] constexpr bool write(auto &&value) noexcept {
+        return static_cast<Impl *>(this)->writeImpl(std::forward<decltype(value)>(value));
+    }
+};
+
+}// namespace kf::internal
+
+namespace kf::mixin {
+
+/// @brief Add `write(auto &&) -> bool`
+/// @tparam Impl Writable mixin implementation class
+/// @tparam T Value type
+template<typename Impl, typename T> struct Writable;
+
+template<typename Impl, typename T> struct Writable : internal::WritableBase<Impl, T> {};
+
+// TODO: char specialization
+
+}// namespace kf::mixin
+
+#define KF_IMPL_WRITABLE(...) friend struct ::kf::internal::WritableBase<__VA_ARGS__>
