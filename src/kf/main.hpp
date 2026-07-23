a28325befe65b30bcca7a002a99118c34cbb575c
KiraFlux <pret2titio1n@gmail.com>
2026-07-23 16:14:50 +0300

    feat(core): add AppIO abstraction for unified binary I/O on Arduino and native

--- src/kf/main.hpp
+++ src/kf/main.hpp
@@ -4,7 +4,15 @@
 #pragma once
 
 #include "kf/Logger.hpp"
+#include "kf/Result.hpp"
+#include "kf/Slice.hpp"
 #include "kf/StringView.hpp"
+#include "kf/concepts.hpp"
+#include "kf/mixin/BinaryReadable.hpp"
+#include "kf/mixin/BinaryWritable.hpp"
+#include "kf/mixin/NonCopyable.hpp"
+
+// autoconfig
 
 #ifndef KF_CONFIG_MAIN_LOGGER_KEY
 #define KF_CONFIG_MAIN_LOGGER_KEY "main"
@@ -18,77 +26,280 @@
 #define KF_CONFIG_SERIAL_BAUDRATE 115200
 #endif
 
+//
+
+#ifdef ARDUINO
+
+#include <Arduino.h>
+
+#define KF_PLATFORM_APP_IO_READ_CHAR_FAILED (-1)
+#define KF_PLATFORM_APP_FUNCTION_DECLARATION void setup()
+#define KF_PLATFORM_APP_INIT() Serial.begin((KF_CONFIG_SERIAL_BAUDRATE))
+#define KF_PLATFORM_APP_QUIT()
+
+void loop() {}// not used
+
+#else
+
+#include <fcntl.h>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+#define KF_PLATFORM_APP_IO_READ_CHAR_FAILED EOF
+#define KF_PLATFORM_APP_FUNCTION_DECLARATION int main()
+
+inline termios kf_original_tty;
+
+#define KF_PLATFORM_APP_IO_READ_CHAR_FAILED EOF
+#define KF_PLATFORM_APP_FUNCTION_DECLARATION int main()
+
+#define KF_PLATFORM_APP_INIT()                                                                   \
+    do {                                                                                         \
+        if (::isatty(STDIN_FILENO)) {                                                            \
+            /* 1. enable raw mode (like arduino UART) */                                         \
+            ::tcgetattr(STDIN_FILENO, &kf_original_tty);                                         \
+            termios raw = kf_original_tty;                                                       \
+            raw.c_lflag &= ~(ICANON | ECHO);                                                     \
+            raw.c_cc[VMIN] = 0;  /* 0 - nonblocking is no bytes */                               \
+            raw.c_cc[VTIME] = 0; /* no timeouts */                                               \
+            ::tcsetattr(STDIN_FILENO, TCSANOW, &raw);                                            \
+                                                                                                 \
+            /* 2. change stdin handler to async mode */                                          \
+            int flags = ::fcntl(STDIN_FILENO, F_GETFL, 0);                                       \
+            ::fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);                                  \
+        }                                                                                        \
+        /* Disable sync iostream with C functions for correct std::cin.get() work in raw mode */ \
+        std::ios_base::sync_with_stdio(false);                                                   \
+    } while (0)
+
+#define KF_PLATFORM_APP_QUIT() (                                                                                  \
+    ::isatty(STDIN_FILENO) ? (                                                                                    \
+                                 ::fcntl(STDIN_FILENO, F_SETFL, ::fcntl(STDIN_FILENO, F_GETFL, 0) & ~O_NONBLOCK), \
+                                 ::tcsetattr(STDIN_FILENO, TCSANOW, &kf_original_tty))                            \
+                           : 0,                                                                                   \
+    0)
+
+#endif
+
 namespace kf {
+
+namespace internal {
+
+enum class AppIoError : char {
+    ReadFailed,
+    WriteFailed,
+};
+
+using AppIoWriteResult = Result<void, AppIoError>;
+
+struct AppIO :
+
+    mixin::NonCopyable,
+    mixin::BinaryReadable<AppIO, AppIoError>,
+    mixin::BinaryWritable<AppIO, AppIoWriteResult>
+
+{
+    using Self = AppIO;
+
+    using Error = AppIoError;
+
+    constexpr AppIO() noexcept = default;
+
+    [[nodiscard]] usize availableForRead() noexcept {
+        return (
+#ifdef ARDUINO
+            Serial.available()
+#else
+            std::cin.rdbuf()->in_avail()
+#endif
+        );
+    }
+
+    [[nodiscard]] usize availableForWrite() noexcept {
+#ifdef ARDUINO
+        auto const ret = Serial.availableForWrite();
+        // Arduino Stream returns size 0 if "may write any size"
+        return (0 == ret) ? static_cast<usize>(-1) : ret;
+#else
+        return std::cout.good() ? static_cast<usize>(-1) : 0;
+#endif
+    }
+
+    void flush() noexcept {
+#ifdef ARDUINO
+        Serial.flush();
+#else
+        (void) std::cout.flush();
+#endif
+    }
+
+private:
+    KF_IMPL_BINARY_READABLE(Self, Error);
+
+    auto readBufferImpl(Slice<u8> buffer) noexcept -> Result<Slice<u8 const>, Error> {
+        if (buffer.empty()) {
+            return ok(Slice<u8 const>{});
+        }
+
+        auto const to_read = buffer.length();
+        usize bytes_read;
+
+#ifdef ARDUINO
+        bytes_read = Serial.readBytes(reinterpret_cast<char *>(buffer.data()), to_read);
+#else
+        std::cin.read(reinterpret_cast<char *>(buffer.data()), to_read);
+        bytes_read = static_cast<usize>(std::cin.gcount());
+#endif
+
+        if (bytes_read == 0) {
+            return error(Error::ReadFailed);
+        }
+
+        return ok(Slice<u8 const>{buffer.data(), bytes_read});
+    }
+
+    template<trivial T> auto readPacketImpl() -> Result<T, Error> {
+        constexpr auto to_read = sizeof(T);
+
+        if constexpr (to_read == sizeof(u8)) {
+            int const c = (
+#ifdef ARDUINO
+                Serial.read()
+#else
+                std::cin.get()
+#endif
+            );
+
+            if (KF_PLATFORM_APP_IO_READ_CHAR_FAILED == c) {
+                return error(Error::ReadFailed);
+            } else {
+                return ok(static_cast<T>(c));
+            }
+        } else {
+            T packet;
+
+            usize bytes_read;
+#ifdef ARDUINO
+            bytes_read = Serial.readBytes(reinterpret_cast<u8 *>(&packet), to_read);
+#else
+            std::cin.read(reinterpret_cast<char *>(&packet), to_read);
+            bytes_read = static_cast<usize>(std::cin.gcount());
+#endif
+            if (to_read == bytes_read) {
+                return ok(packet);
+            } else {
+                return error(Error::ReadFailed);
+            }
+        }
+    }
+
+    KF_IMPL_BINARY_WRITABLE(Self, AppIoWriteResult);
+
+    AppIoWriteResult writeBufferImpl(Slice<u8 const> buffer) noexcept {
+        if (not buffer.empty()) {
+            auto const to_write = buffer.length();
+
+            if (
+#ifdef ARDUINO
+                Serial.write(buffer.data(), to_write) != to_write
+#else
+                not std::cout.write(reinterpret_cast<char const *>(buffer.data()), to_write).good()
+#endif
+            ) {
+                return error(Error::WriteFailed);
+            }
+        }
+
+        return ok();
+    }
+
+    AppIoWriteResult writePacketImpl(trivial auto const &packet) noexcept {
+        constexpr auto to_write = sizeof(packet);
+
+#ifdef ARDUINO
+        usize written;
+
+        if constexpr (to_write == sizeof(u8)) {
+            written = Serial.write(static_cast<u8>(packet));
+        } else {
+            written = Serial.write(reinterpret_cast<u8 const *>(&packet), to_write);
+        }
+
+        if (to_write != written)
+#else
+
+        if constexpr (to_write == sizeof(u8)) {
+            std::cout.put(static_cast<char>(packet));
+        } else {
+            std::cout.write(reinterpret_cast<char const *>(&packet), to_write);
+        }
+
+        if (not std::cout.good())
+#endif
+        {
+            return error(Error::WriteFailed);
+        } else {
+            return ok();
+        }
+    }
+
+    AppIoWriteResult writeMixedImpl(trivial auto const &header, Slice<u8 const> buffer) noexcept {
+        KF_TRY(this->writePacket(header));
+        return this->writeBuffer(buffer);
+    }
+};
+
+}// namespace internal
 
 /// @brief App Context
 struct Init {
-    // TODO: add io - (platform-depended binary IO class instance)
 
-    /// @brief application logger
+    /// @brief Platform-depended standard binary Input/Output Stream
+    internal::AppIO io;
+
+    /// @brief Application Logger
     Logger logger;
 };
 
 /// @brief KiraFlux Toolkit application entry point
 void main(Init &init);
 
-namespace internal {
+}// namespace kf
 
-void launch() noexcept {
+KF_PLATFORM_APP_FUNCTION_DECLARATION {
+    KF_PLATFORM_APP_INIT();
+
     static char main_logger_buffer[(KF_CONFIG_MAIN_LOGGER_BUFFER_LENGTH)]{};
 
-    Init init{
-        .logger = Logger{
+    static kf::Init init{
+        .io = {},
+        .logger = kf::Logger{
             (KF_CONFIG_MAIN_LOGGER_KEY),
             {main_logger_buffer},
         },
     };
 
-    // TODO: set logger writer here (use init.io)
+    kf::Logger::writer = [](kf::StringView str) {
+        (void) init.io.writeBuffer({reinterpret_cast<kf::u8 const *>(str.data()), str.length()});
+        init.io.flush();
+    };
 
     init.logger.info("KiraFlux Toolkit v0.3.5");
 
     main(init);
+
+    return KF_PLATFORM_APP_QUIT();
 }
 
-}// namespace internal
+// cleanup
 
-}// namespace kf
+#undef KF_PLATFORM_APP_IO_READ_CHAR_FAILED
 
-#ifdef ARDUINO
+#undef KF_PLATFORM_APP_FUNCTION_DECLARATION
+#undef KF_PLATFORM_APP_INIT
+#undef KF_PLATFORM_APP_QUIT
 
-#include <Arduino.h>
-
-void setup() {
-    Serial.begin((KF_CONFIG_SERIAL_BAUDRATE));
-
-    kf::Logger::writer = [](kf::StringView str) {
-        if (not str.empty()) {
-            Serial.write(str.data(), str.length());
-            Serial.flush();
-        }
-    };
-
-    kf::internal::launch();
-}
-
-void loop() {}
-
-#else
-
-#include <iostream>
-
-int main() {
-
-    kf::Logger::writer = [](kf::StringView str) {
-        if (not str.empty()) {
-            std::cout.write(str.data(), str.length());
-            std::cout.flush();
-        }
-    };
-
-    kf::internal::launch();
-
-    return 0;
-}
-
-#endif
+#undef KF_CONFIG_MAIN_LOGGER_KEY
+#undef KF_CONFIG_MAIN_LOGGER_BUFFER_LENGTH
+#undef KF_CONFIG_SERIAL_BAUDRATE
