d104e09407c4ddb7cd039f7acacf01a51a518618
KiraFlux <pret2titio1n@gmail.com>
2026-07-26 23:52:02 +0300

    feat(mixin): add ReadAvailable/WriteAvailable

---
+++ src/kf/mixin/ReadAvailable.hpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "kf/primitives.hpp"
+
+namespace kf::mixin {
+
+struct ReadAvailableTag {};
+
+/// @brief Adds `availableForRead()` to query how many bytes/items can be read without blocking.
+/// @tparam Impl Implementation class with `usize availableForReadImpl() const noexcept` method.
+/// @note The value may change between calls; intended for non‑blocking polling.
+template<typename Impl> struct ReadAvailable : ReadAvailableTag {
+
+    /// @brief Get the number of bytes/items currently available to read.
+    [[nodiscard]] usize availableForRead() const noexcept {
+        return static_cast<Impl const *>(this)->availableForReadImpl();
+    }
+};
+
+}// namespace kf::mixin
+
+#define KF_IMPL_READ_AVAILABLE(...) friend struct ::kf::mixin::ReadAvailable<__VA_ARGS__>
