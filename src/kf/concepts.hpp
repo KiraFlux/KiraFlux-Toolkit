651512a5384ff3224b3789f5f7177e025954dd73
KiraFlux <pret2titio1n@gmail.com>
2026-08-14 02:34:12 +0300

    feat: add callable concept

--- src/kf/concepts.hpp
+++ src/kf/concepts.hpp
@@ -23,4 +23,13 @@ concept enum_type = std::is_enum_v<E>;
 template<typename T>
 concept float_type = std::is_floating_point_v<T>;
 
+/// @brief Concept for callable objects with a specific signature.
+/// @tparam F    The callable type.
+/// @tparam Ret  Expected return type.
+/// @tparam Args Argument types the callable must accept.
+template<typename F, typename Ret, typename... Args>
+concept callable = requires(F f, Args... args) {
+    { f(args...) } -> std::same_as<Ret>;
+};
+
 }// namespace kf
