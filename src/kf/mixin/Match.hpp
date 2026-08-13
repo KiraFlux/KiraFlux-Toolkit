1dc1daad83157cc31e9443fad661027e1147a5b4
KiraFlux <pret2titio1n@gmail.com>
2026-08-14 02:35:25 +0300

    fix(mixin/Match): make T reachable via MatchingType

--- src/kf/mixin/Match.hpp
+++ src/kf/mixin/Match.hpp
@@ -9,15 +9,17 @@ namespace kf::mixin {
 
 struct MatchTag {};
 
-/// @brief Adds `match(T)` to check if the object matches a given value.
+/// @brief Adds `match(T) -> bool` to check if the object matches a given value.
 /// @tparam Impl Match implementation class with `bool matchImpl(T value) noexcept` method
 /// @tparam T   The type of the matched value.
 template<typename Impl, typename T> struct Match : MatchTag {
 
+    using MatchingType = T;
+
     /// @brief Compare the object against the provided value.
     /// @param value The value to match against.
     /// @return `true` if the object matches, `false` otherwise.
-    [[nodiscard]] bool match(T value) noexcept {
+    [[nodiscard]] bool match(MatchingType value) noexcept {
         return static_cast<Impl *>(this)->matchImpl(value);
     }
 };
